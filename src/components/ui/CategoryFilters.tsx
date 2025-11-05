28ed3b854f55d93ba8a8f0ccbca7c632b42c4dcb
rushil <rushil13579@gmail.com>
2025-11-05 23:58:52 +0530

    Create Event Form

--- src/components/ui/CategoryFilters.tsx
+++ src/components/ui/CategoryFilters.tsx
@@ -121,7 +121,7 @@ export default function CategoryFilters() {
             setIsDatesOpen(false);
             setIsDurationOpen(false);
           }}
-          className={`flex items-center gap-2 rounded-full px-2 py-2 text-sm font-medium whitespace-nowrap border transition-all
+          className={`flex items-center gap-2 rounded-full px-4 py-2 text-sm font-medium whitespace-nowrap border transition-all
               ${
                 selectedTypes.length > 0
                   ? 'bg-black text-background border-transparent'
