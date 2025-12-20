a7012f0ace71aa646efc13cd87523198cf576e42
Dhruv Personal <chauhandhruv351@gmail.com>
2025-12-20 23:07:54 +0530

    use axios for profile

--- src/api/profile.ts
+++ src/api/profile.ts
@@ -3,3 +3,4 @@ import { ProfileData } from '@/types';
 
 export const getProfile = () => api.get('/profile');
 export const updateProfile = (data: Partial<ProfileData>) => api.patch('/profile', data);
+export const createProfile = (data: Partial<ProfileData>) => api.post('/profile', data);
